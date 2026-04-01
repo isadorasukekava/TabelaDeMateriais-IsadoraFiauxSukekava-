a2342702619807ef18fddb0f607ed4b7290c6dcd
Isadora Fiaux Sukekava <isadorasukekava@gmail.com>
2026-04-01 17:20:52 +0000

    feat: update List.controller.js

--- CAP_TabelaDeMateriais/app/tabelademateriaismname/webapp/controller/List.controller.js
+++ CAP_TabelaDeMateriais/app/tabelademateriaismname/webapp/controller/List.controller.js
@@ -1,10 +1,41 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
-
-    return Controller.extend("tabelademateriaismname.controller.List", {
-        onInit() {
-        }
-    });
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], (Controller, MessageToast) => {
+   "use strict";
+   return Controller.extend("tabelademateriaismname.controller.List", {
+       onInit: function () {
+           this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+           this.oRouter
+               .getTarget("TargetList")
+               .attachDisplay(this.handleRouteMatched, this);
+       },
+       handleRouteMatched: function () {
+           this.createModel();
+           this.carregarDados();
+       },
+       createModel: function () {
+           this.getView().setModel(
+               new sap.ui.model.json.JSONModel({
+                   tableMaterial: []
+               }),
+               "tableMaterial"
+           );
+       },
+       carregarDados: async function () {
+           const response = await fetch("/odata/v4/materiais/Material");
+           const data = await response.json();
+           this.getView().getModel("tableMaterial").setProperty("/tableMaterial", data.value);
+       },
+       onFiltrar: async function () {
+           const quantidade = this.getView().byId("inputQuantidade").getValue();
+           if (!quantidade) {
+               MessageToast.show("Digite uma quantidade!");
+               return;
+           }
+           const response = await fetch(`/odata/v4/materiais/filtroMateriais(quantidade=${quantidade})`);
+           const data = await response.json();
+           this.getView().getModel("tableMaterial").setProperty("/tableMaterial", data.value);
+       }
+   });
 });
